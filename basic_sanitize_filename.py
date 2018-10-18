c050077f71dd1e313ddb712f815345f9eab10497
Fred Duby <fcd1@columbia.edu>
2018-10-18 19:08:09 -0400

    More functionality, some arg parsing. CHECK CODE/FUNCTIONALITY

--- basic_sanitize_filename.py
+++ basic_sanitize_filename.py
@@ -1,16 +1,64 @@
 # assumption: Remediate just filenames, not path to filenames
 # input: absolute path to directory containing filename to remediate
+# fcd1, 18Oct18: add a no-op operation, where it will print out the change,
+# but not actually change the dir/file name.
+# use --dry-run
 
 import sys
 import os
+import argparse
 
-# get arguments. Returns a list, first entry is name of script
-# assuming called as follows: python3 basic_sanitize_filename.py hi
-# rest of arguments should be absolute paths to directories
-args = sys.argv
-print(args)
-args.pop(0)
-print(args)
+def main():
+    print("Entering main()")
+
+    # process arguments
+
+    # get arguments. Returns a list, first entry is name of script
+
+    # assuming called as follows: python3 basic_sanitize_filename.py hi
+    # rest of arguments should be absolute paths to directories
+    # args = sys.argv
+    # print(args)
+    # args.pop(0)
+    # print(args)
+
+    parser = argparse.ArgumentParser(description='Sanitize directory contents (files and subdirs)')
+    parser.add_argument('dirs',
+                        metavar='dir',
+                        nargs='+',
+                        help='directory to sanitize')
+
+    args = parser.parse_args()
+
+    # for each argument
+    for i, directory in enumerate(args.dirs):
+        # only process dir if it's an absolute path and
+        # it's really a directory
+        if ( os.path.isabs(directory) and os.path.isdir(directory) ):
+            print("directory at position " + str(i) + " is " + directory)
+            process_dir(directory)
+        else:
+            print("Kaboom!")
+
+def process_dir(target_dir):
+    # first, clean up paths
+    process_subdir_names(target_dir)
+    # find_files(target_dir)
+
+def process_subdir_names(target_dir):
+    # os.walk takes a topdown parameter which defaults to true.
+    # Need to set it to true so it goes bottom-up.
+    for root, subdirs, files in os.walk(target_dir, topdown=False):
+        for subdir in subdirs:
+            print(subdir)
+            # as_test_just_rename_dir(root,subdir)
+            print(subdir)
+
+def find_files(target_dir):
+    for root, subdirs, files in os.walk(target_dir):
+        for target_file in files:
+            print(os.path.join(root,target_file))
+            process_file(target_file)
 
 def process_file(target_file):
     # separate file into basename and extension
@@ -18,19 +66,12 @@ def process_file(target_file):
     print(basename)
     print(extension)
 
-def process_dir(target_dir):
-    for root, subdirs, files in os.walk(target_dir):
-        for target_file in files:
-            print(os.path.join(root,target_file))
-            process_file(target_file)
+def as_test_just_rename_dir(base_path, target_dir):
+    os.rename(os.path.join(base_path, target_dir),
+              os.path.join(base_path, target_dir + "foo"))
 
-# for each argument
-for arg in args:
-    # only process arg if it's an absolute path and
-    # arg is a dir
-    if ( os.path.isabs(arg) and os.path.isdir(arg) ):
-        print(arg)
-        process_dir(arg)
-    else:
-        print("Kaboom!")
+def validate_name_and_remediate_if_needed(target_name):
+    pass
 
+if __name__ == "__main__":
+    main()
